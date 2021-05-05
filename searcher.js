ec5a6ae7a5423b420bd3cd062635e10fe4182d35
Ethan CHen <ethanctrooper@gmail.com>
2021-05-05 12:30:34 -0700

    semi functional caret position getter, non-functional setter

--- searcher.js
+++ searcher.js
@@ -1,9 +1,10 @@
 function search(highlightArrayInput){
 
+  var selBox = document.getElementById("inputBox_CE")
   var rawInput = document.getElementById("inputBox_CE").innerText
 
   //get info of current caret position
-  
+  var currentCaretPos = getCaretCharacterOffsetWithin(selBox)
 
   //next project: try to get the thing to highlight the text in the input box as it's being inputted
 
@@ -35,6 +36,10 @@ function search(highlightArrayInput){
 
   //reset position of caret after replacement
 
+  var selNode = getSelectionStart()
+  var offset = getCaretCharacterOffsetWithin(selNode)
+  setCaret(selNode, offset)
+
 
   //End of search()
 
@@ -53,4 +58,63 @@ function addToHighlights(oldInput, regexExpression){
 function markAdder(match, p1, offset, inputString){
   var mark = "<mark>"
   return mark.concat(match, "</mark>")
+}
+
+//from https://stackoverflow.com/questions/4811822/get-a-ranges-start-and-end-offsets-relative-to-its-parent-container/4812022#4812022
+function getCaretCharacterOffsetWithin(element) {
+    var caretOffset = 0;
+    var doc = element.ownerDocument || element.document;
+    var win = doc.defaultView || doc.parentWindow;
+    var sel;
+    if (typeof win.getSelection != "undefined") {
+        sel = win.getSelection();
+        if (sel.rangeCount > 0) {
+            var range = win.getSelection().getRangeAt(0);
+            var preCaretRange = range.cloneRange();
+            preCaretRange.selectNodeContents(element);
+            preCaretRange.setEnd(range.endContainer, range.endOffset);
+            caretOffset = preCaretRange.toString().length;
+        }
+    } else if ( (sel = doc.selection) && sel.type != "Control") {
+        var textRange = sel.createRange();
+        var preCaretTextRange = doc.body.createTextRange();
+        preCaretTextRange.moveToElementText(element);
+        preCaretTextRange.setEndPoint("EndToEnd", textRange);
+        caretOffset = preCaretTextRange.text.length;
+    }
+    return caretOffset;
+}
+
+function testCaretPos(){
+  var selBox = document.getElementById("inputBox_CE")
+  var caretBox = document.getElementById("caretPosOutput")
+
+  var selNode = getSelectionStart()
+
+  console.log(getCaretCharacterOffsetWithin(selBox) + "LKAJHSFKLAUSHFKJANLJFHDLAKSD")
+
+  caretBox.innerHTML = getCaretCharacterOffsetWithin(selNode)
+  console.log(getSelectionStart())
+}
+
+document.body.onkeyup = testCaretPos;
+document.body.onmouseup = testCaretPos;
+
+//from https://stackoverflow.com/questions/6249095/how-to-set-caretcursor-position-in-contenteditable-element-div
+function setCaret(node, offset) {
+    var el = document.getElementById("inputBox_CE")
+    var range = document.createRange()
+    var sel = window.getSelection()
+
+    range.setStart(node, offset)
+    range.collapse(true)
+
+    sel.removeAllRanges()
+    sel.addRange(range)
+}
+
+//from https://stackoverflow.com/questions/1197401/get-element-node-at-caret-position-in-contenteditable
+function getSelectionStart() {
+   var node = document.getSelection().anchorNode;
+   return (node.nodeType == 3 ? node.parentNode : node);
 }
